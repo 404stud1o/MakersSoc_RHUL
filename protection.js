d50a7ababd93a79c81a468f9e16f7168327073c7
Henry Li <73312050+404stud1o@users.noreply.github.com>
2025-02-04 06:39:52 +0000

    script update

--- protection.js
+++ protection.js
@@ -1,3 +1,16 @@
+// function toggleVisibility() {
+//     const passwordField = document.getElementById("password");
+//     const toggleButton = document.getElementById("togglePassword");
+    
+//     if (passwordField.type === "password") {
+//         passwordField.type = "text";
+//         toggleButton.textContent = "👁️" ; //char code &#x1F441; // Visible state
+//     } else {
+//         passwordField.type = "password";
+//         toggleButton.textContent = "&#x1F441;"; // Hidden state
+//     }
+// }
+
 function checkPassword() {
     const password = document.getElementById("password").value;
     const correctPassword = "3kM4?0c5UH1r"; // password
